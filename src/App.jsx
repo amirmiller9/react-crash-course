ce320f093f2401c2fa72e499e0d30bdade7a25a5
Amir Miller <amir.miller@quicklizard.com>
2026-01-11 23:28:23 +0200

    Refactor PostsList to manage local state and integrate modal for new post creation

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,9 @@ import { useState } from 'react';
 
 import PostsList from './components/PostsList'
 import MainHeader from './components/MainHeader'
-import Modal from './components/Modal'
-import NewPost from './components/NewPost'
 
 function App() {
   const [modalIsVisible, setModalIsVisible] = useState(false);
-  const [posts, setPosts] = useState([
-    { author: "Amir", body: "React is awesome!" },
-    { author: "Junie", body: "I'm helping build this app." },
-    { author: "Vite", body: "I'm the build tool." },
-  ]);
 
   function showModalHandler() {
     setModalIsVisible(true);
@@ -21,20 +14,14 @@ function App() {
     setModalIsVisible(false);
   }
 
-  function addPostHandler(postData) {
-    setPosts((existingPosts) => [postData, ...existingPosts]);
-  }
-
   return (
     <>
       <MainHeader onCreatePost={showModalHandler} />
       <main>
-        {modalIsVisible && (
-          <Modal onClose={hideModalHandler}>
-            <NewPost onCancel={hideModalHandler} onAddPost={addPostHandler} />
-          </Modal>
-        )}
-        <PostsList posts={posts} />
+        <PostsList
+          isModalVisible={modalIsVisible}
+          onStopPosting={hideModalHandler}
+        />
       </main>
     </>
   )
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
+
 import Post from './Post';
+import NewPost from './NewPost';
+import Modal from './Modal';
 import classes from './PostsList.module.css';
 
-function PostsList({ posts }) {
+function PostsList({ isModalVisible, onStopPosting }) {
+  const [posts, setPosts] = useState([
+    { author: "Amir", body: "React is awesome!" },
+    { author: "Junie", body: "I'm helping build this app." },
+    { author: "Vite", body: "I'm the build tool." },
+  ]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
   return (
     <>
+      {isModalVisible && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      )}
       {posts.length > 0 && (
         <ul className={classes.posts}>
-          {posts.map((post, index) => (
-            <Post key={index} author={post.author} body={post.body} />
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
