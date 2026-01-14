f032bf85b8d333479f7edc9326780b36b00de86f
Amir Miller <amir.miller@quicklizard.com>
2026-01-14 10:46:03 +0200

    chore: untrack .next directory and update build artifacts in gitignore

--- .next/dev/static/chunks/src_app_page_jsx_8e9f1618._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_dist_75831940._.js",
-  "static/chunks/src_components_7c4c6d0e._.css"
-],
-    source: "dynamic"
-});
