d556b6bf0da8f3ddde6e4a3151bf73c3b160ce94
Amir Miller <amir.miller@quicklizard.com>
2026-01-15 13:45:49 +0200

    style: update ImagePicker component styles for improved aesthetics and usability

--- src/components/ImagePicker.jsx
+++ src/components/ImagePicker.jsx
@@ -52,7 +52,6 @@ export default function ImagePicker({ label, name }) {
           name={name}
           ref={imageInput}
           onChange={handleImageChange}
-          required
         />
         <button
           className={classes.button}
