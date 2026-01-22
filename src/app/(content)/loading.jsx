32132fdf31ffb02e6a65970eb7e197336eb8599c
Amir Miller <amir.miller@quicklizard.com>
2026-01-22 15:40:02 +0200

    feat: implement posts management with database integration and new post form

--- src/app/(content)/loading.jsx
+++ src/app/(content)/loading.jsx
@@ -3,7 +3,7 @@ import classes from './loading.module.css';
 export default function Loading() {
   return (
     <div className={classes.loading}>
-      <p>Loading content...</p>
+      <p>MilleR Loading content...</p>
       <div className={classes.spinner}></div>
     </div>
   );
